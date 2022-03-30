e75a172b7c3ef5f83d590216f922531b3cc3683d
youmemonk <ukeym160@gmail.com>
2022-03-30 17:14:08 +0530

    @env issue fix

---
+++ components/Map.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import MapView from "react-native-maps";
+import tw from "twrnc";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
+
+const Map = () => {
+  const origin = useSelector(selectOrigin);
+
+  return (
+    <MapView
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    />
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({});
