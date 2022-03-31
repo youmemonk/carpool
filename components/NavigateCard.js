dc50df487a524e6760b7d12b0094d9aa1a1b611a
youmemonk <ukeym160@gmail.com>
2022-03-31 16:55:50 +0530

    Navigations Card

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation, useNavigationState } from "@react-navigation/native";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, MU!</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where To?"
+            styles={toInputBoxStyles}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default NavigateCard
+export default NavigateCard;
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+const RideOptionsCard = () => {
+  return (
+    <SafeAreaView>
+      <Text>Pee Pee Poo Poo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default RideOptionsCard;
+
+const styles = StyleSheet.create({});
